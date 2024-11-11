ba49cd6199494b9ac2a7d71ac0418d7a76e7300f
Aleksandra Ivanova <sashaivanova90@mail.ru>
2024-11-11 14:22:57 +0300

    Change Character.js

--- src/js/class/Character.js
+++ src/js/class/Character.js
@@ -14,32 +14,5 @@ export default class Character {
         this.level = level;
         this.attack = attack;
         this.defence = defence;
-
-        // switch (type) {
-        //     case 'Bowman':
-        //         this.attack = 25;
-        //         this.defence = 25;
-        //         break
-        //     case 'Swordsman':
-        //         this.attack = 40;
-        //         this.defence = 10;
-        //         break
-        //     case 'Magician':
-        //         this.attack = 10;
-        //         this.defence = 40;
-        //         break
-        //     case 'Daemon':
-        //         this.attack = 10;
-        //         this.defence = 40;
-        //         break
-        //     case 'Undead':
-        //         this.attack = 25;
-        //         this.defence = 25;
-        //         break
-        //     case 'Zombie':
-        //         this.attack = 40;
-        //         this.defence = 10;
-        //         break
-        // }
     }
 }
